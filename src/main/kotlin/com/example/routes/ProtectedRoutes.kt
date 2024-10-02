7ed2897136eca90cfaa64a8d401d2c4097067e9a
YouMayknow <successfulkaran@gmail.com>
2024-10-02 16:42:33 +0530

    protected routes setup

--- src/main/kotlin/com/example/routes/ProtectedRoutes.kt
+++ src/main/kotlin/com/example/routes/ProtectedRoutes.kt
@@ -1,8 +1,10 @@
 package com.example.routes
 
+import io.ktor.http.*
 import io.ktor.server.auth.*
 import io.ktor.server.auth.jwt.*
 import io.ktor.server.request.*
+import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Route.protectedRoutes() {
@@ -10,6 +12,7 @@ fun Route.protectedRoutes() {
         get("/notes/new") {
             val principal = call.principal<JWTPrincipal>()
             val username = principal!!.payload.getClaim("username").asString()
+            call.respond(HttpStatusCode.OK, "hey you got the inner information beautifully : $username", )
         }
     }
 }
